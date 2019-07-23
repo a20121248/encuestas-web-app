f363346816b92440aa816454b11ff85314697b96
Javier Monzón <javier.monzon@pucp.pe>
2019-07-23 11:08:23 -0500

    cambios front

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule}from '@angular/material/card';
+import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
