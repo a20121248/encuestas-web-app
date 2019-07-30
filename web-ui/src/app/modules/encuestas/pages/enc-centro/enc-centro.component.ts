5aec28bb51c719608fb9de50555f4965e11777c2
Rodrigo <rodrigo.albarracin88@gmail.com>
2019-07-30 17:26:00 -0500

    Cambios Centros

--- web-ui/src/app/modules/encuestas/pages/enc-centro/enc-centro.component.ts
+++ web-ui/src/app/modules/encuestas/pages/enc-centro/enc-centro.component.ts
@@ -1,40 +1,54 @@
+import { Component, OnInit } from "@angular/core";
+import { ViewChild } from "@angular/core";
+import { Location } from "@angular/common";
 
-import { Component, OnInit } from '@angular/core';
-import { ViewChild } from '@angular/core';
-import { Location } from '@angular/common';
 import { Router, ActivatedRoute } from "@angular/router";
+import swal from "sweetalert2";
 
 //-------------------COMPONENTES LOCALES----------------------------------
 
-import { Centro } from  '../../../../shared/models/centro';
-import { CentroComponent } from '../../components/centro/centro.component';
-import { CentroService } from 'src/app/shared/services/centro.service';
-
+import { Centro } from "../../../../shared/models/centro";
+import { CentroComponent } from "../../components/centro/centro.component";
+import { CentroService } from "src/app/shared/services/centro.service";
+import { Encuesta } from "src/app/shared/models/encuesta";
+import { UsuarioDatosComponent } from "src/app/shared/components/usuario-datos/usuario-datos.component";
+import { Justificacion } from "src/app/shared/models/justificacion";
+import { JustificacionComponent } from "src/app/shared/components/justificacion/justificacion.component";
 
 @Component({
-  selector: 'app-enc-centro',
-  templateUrl: './enc-centro.component.html',
-  styleUrls: ['./enc-centro.component.css']
+  selector: "app-enc-centro",
+  templateUrl: "./enc-centro.component.html",
+  styleUrls: ["./enc-centro.component.css"]
 })
 export class EncCentroComponent implements OnInit {
-  lstCentros: Centro[] ;
-  titulo = 'Herramienta de encuestas';
+  lstCentros: Centro[];
+  titulo = "Herramienta de encuestas";
 
   constructor(
     private centroService: CentroService,
     private activatedRoute: ActivatedRoute,
-    private location: Location ) { }
+    private location: Location
+  ) {}
 
-  @ViewChild(CentroComponent, {static: false})  centroComponent: CentroComponent ;
+  @ViewChild(CentroComponent, { static: false })
+  centroComponent: CentroComponent;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   guardarEncuesta() {
-    this.lstCentros = this.centroComponent.getLstCentro();
-    this.centroService.postRespuesta (this.lstCentros);
+    this.encuesta = new Encuesta();
+    this.encuesta.lstItems = this.epsComponent.lstEps;
+    this.encuesta.justificacion = null;
+    this.encuesta.observaciones = null;
+    this.epsService
+      .guardarEncuesta(this.encuesta, this.posicionCodigo)
+      .subscribe(response => console.log(response), err => console.log(err));
+
+    // goBack() {
+    //   this.location.back();
+    // }
   }
-  goBack(){
+  goBack() {
     this.location.back();
   }
 }
