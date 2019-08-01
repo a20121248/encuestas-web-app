1865837035c1177a87eecf682d08fb0255cf72bd
Briggette Olenka Roman Huaytalla <briggette.olenka.ro1@SCMS.ms.corp>
2019-08-01 18:42:33 -0500

    Mantenimiento: Esquema.

--- web-ui/src/app/modules/mantenimientos/pages/mantenimientos/mantenimientos.component.ts
+++ web-ui/src/app/modules/mantenimientos/pages/mantenimientos/mantenimientos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mantenimientos.component.css']
 })
 export class MantenimientosComponent implements OnInit {
-
+  titulo = 'Módulo de Mantenimiento';
   constructor() { }
 
   ngOnInit() {
