861094a88ec241c638f8d3b63e7b161f67dc138e
Javier Monzón <javier.monzon@pucp.pe>
2019-08-07 19:02:03 -0500

    agrupacion centros

--- web-ui/src/app/modules/encuestas/components/centro/centro.component.ts
+++ web-ui/src/app/modules/encuestas/components/centro/centro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Centro } from 'src/app/shared/models/centro';
 import { Usuario } from 'src/app/shared/models/usuario';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-form-centro',
@@ -13,14 +14,105 @@ export class CentroComponent implements OnInit {
   @Input() lstCentros: Centro[];
   @Input() usuarioSeleccionado: Usuario;
   dcCentro = ['codigo', 'nombre', 'nivel', 'porcentaje'];
+  dataSource = new MatTableDataSource<Centro | Group>([]);
+  groupByColumns: string[] = ['nivel'];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    this.lstCentros = [];
+    this.dataSource.data = this.addGroups(this.lstCentros,this.groupByColumns);
+    this.dataSource.filterPredicate = this.customFilterPredicate.bind(this);
+  }
+
+  customFilterPredicate(data: Centro | Group, filter: string): boolean {
+    return (data instanceof Group) ? data.visible : this.getDataRowVisible(data);
+  }
+
+  getDataRowVisible(data: Centro): boolean {
+    const groupRows = this.dataSource.data.filter(
+      row => {
+        if (!(row instanceof Group)) return false;
+
+        let match = true;
+        this.groupByColumns.forEach(
+          column => {
+            if (!row[column] || !data[column] || row[column] !== data[column]) match = false;
+          }
+        );
+        return match;
+      }
+    );
+
+    if (groupRows.length === 0) return true;
+    if (groupRows.length > 1) throw "Data row is in more than one group!";
+    const parent = <Group>groupRows[0];  // </Group> (Fix syntax coloring)
+
+    return parent.visible && parent.expanded;
+  }
+
+  groupHeaderClick(row) {
+    row.expanded = !row.expanded
+    this.dataSource.filter = performance.now().toString();  // hack to trigger filter refresh
+  }
+
+  addGroups(data: any[], groupByColumns: string[]): any[] {
+    var rootGroup = new Group();
+    return this.getSublevel(data, 0, groupByColumns, rootGroup);
+  }
+
+  getSublevel(data: any[], level: number, groupByColumns: string[], parent: Group): any[] {
+    // Recursive function, stop when there are no more levels.
+    if (level >= groupByColumns.length)
+      return data;
+
+    var groups = this.uniqueBy(
+      data.map(
+        row => {
+          var result = new Group();
+          result.level = level + 1;
+          result.parent = parent;
+          for (var i = 0; i <= level; i++)
+            result[groupByColumns[i]] = row[groupByColumns[i]];
+          return result;
+        }
+      ),
+      JSON.stringify);
+
+    const currentColumn = groupByColumns[level];
+
+    var subGroups = [];
+    groups.forEach(group => {
+      let rowsInGroup = data.filter(row => group[currentColumn] === row[currentColumn])
+      let subGroup = this.getSublevel(rowsInGroup, level + 1, groupByColumns, group);
+      subGroup.unshift(group);
+      subGroups = subGroups.concat(subGroup);
+    })
+    return subGroups;
+  }
+
+  uniqueBy(a, key) {
+    var seen = {};
+    return a.filter(function (item) {
+      var k = key(item);
+      return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+    })
+  }
+
+  isGroup(index, item): boolean {
+    return item.level;
+  }
+
+  getTotalPorcentaje() {
+    return this.lstCentros.map(t => t.porcentaje).reduce((acc, value) => acc + value, 0);
+  }
 }
 
-  getTotalPorcentaje(){
-    return this.lstCentros.map(t => t.porcentaje).reduce((acc, value) => acc + value, 0);
+export class Group {
+  level: number = 0;
+  parent: Group;
+  expanded: boolean = true;
+  get visible(): boolean {
+    return !this.parent || (this.parent.visible && this.parent.expanded);
   }
 }
