1bf22bc8b72b757a86713f28ed4159b43500c804
Briggette Román <briggette.roman@gmail.com>
2019-08-14 20:59:59 -0500

    Servicio Validar: patrón de número.

--- web-ui/src/app/shared/services/custom-validators.service.ts
+++ web-ui/src/app/shared/services/custom-validators.service.ts
@@ -7,6 +7,15 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class CustomValidatorsService extends Validators{
 
+  static validarPatronPorcentaje(control:FormControl){
+    const PTRN_PORCENTAJE = /^[\d]{0,3}[.,]{0,1}[\d]{0,2}$/;
+    if(!PTRN_PORCENTAJE.test(control.value)){
+      return {invalidNumber:true}
+    } else {
+      return null;
+    }
+  }
+
   static validarVacio(control:FormControl){
     if(control.value == 0 || control.value==null){
       return{vacio:true}
