ff34dc9b99e6c883c4720de98621e9c2e2dd0d0d
Briggette Román <briggette.roman@gmail.com>
2019-08-16 20:11:52 -0500

    Centros: Validaciones de form. Validaciones de flujo.

--- web-ui/src/app/modules/encuestas/pages/enc-centro/enc-centro.component.ts
+++ web-ui/src/app/modules/encuestas/pages/enc-centro/enc-centro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ElementRef, Renderer2 } from "@angular/core";
 import { ViewChild } from "@angular/core";
 import { Location } from "@angular/common";
 
@@ -17,6 +17,7 @@ import { JustificacionComponent } from 'src/app/shared/components/justificacion/
 import { Usuario } from 'src/app/shared/models/usuario';
 import { UsuarioService } from 'src/app/shared/services/usuario.service';
 import { Title } from '@angular/platform-browser';
+import { SharedFormService } from 'src/app/shared/services/shared-form.service';
 
 @Component({
   selector: 'app-enc-centro',
@@ -31,19 +32,27 @@ export class EncCentroComponent implements OnInit {
   posicionCodigo: string;
   encuesta: Encuesta;
   usuarioSeleccionado: Usuario;
+  estadoCentros: boolean;
+  estadoJustificacion:boolean;
+  haGuardado: boolean;
+
   @ViewChild(CentroComponent, { static: false })
   centroComponent: CentroComponent;
   @ViewChild(JustificacionComponent, { static: false })
   justificacionComponent: JustificacionComponent;
   @ViewChild(UsuarioDatosComponent, { static: false })
   usuarioDatosComponent: UsuarioDatosComponent;
-
+  @ViewChild("btnGuardar",{static: false}) 
+  btnGuardar: ElementRef;
+  
   constructor(
     private activatedRoute: ActivatedRoute,
     private centroService: CentroService,
     private location: Location,
     private usuarioService: UsuarioService,
-    private titleService: Title
+    private titleService: Title,
+    private renderer: Renderer2,
+    private sharedFormService: SharedFormService
   ) {
     this.posicionCodigo = this.activatedRoute.snapshot.paramMap.get('codigo');
     this.usuarioService.getUsuarioByPosicionCodigo(this.posicionCodigo).subscribe(usuario => {
@@ -60,7 +69,26 @@ export class EncCentroComponent implements OnInit {
     this.titleService.setTitle('Encuestas | Centros de costos');
   }
 
+  estadoFormJustificacion(value:boolean){
+    this.estadoJustificacion = value;
+    this.setButtonGuardar();
+  }
+
+  estadoFormCentros(value:boolean){
+    this.estadoCentros = value;
+    this.setButtonGuardar();
+  }
+
+  setButtonGuardar(){
+    if(this.estadoCentros && this.estadoJustificacion){
+      this.renderer.setProperty(this.btnGuardar,"disabled","false");
+    } else {
+      this.renderer.setProperty(this.btnGuardar,"disabled","true");
+    }
+  }
+
   guardarEncuesta() {
+    this.haGuardado = true;
     this.encuesta = new Encuesta();
     this.encuesta.lstItems = this.centroComponent.lstCentros;
     this.encuesta.justificacion = this.justificacionComponent.justificacion;
@@ -75,6 +103,30 @@ export class EncCentroComponent implements OnInit {
   }
 
   goBack() {
-    this.location.back();
+    let form1dirty:boolean;
+    let form2dirty:boolean;
+    this.sharedFormService.form1Actual.subscribe(data => {
+      form1dirty = data.dirty;
+    });
+    this.sharedFormService.form2Actual.subscribe(data => {
+      form2dirty = data.dirty;
+    });
+    console.log(form1dirty);
+    console.log(form2dirty);
+    if(this.haGuardado){
+      this.location.back();
+    } else {
+      if( form1dirty || form2dirty ){
+        swal.fire({
+          title: 'Cambios detectados',
+          text: "Primero guarde antes de continuar.",
+          type: "warning"
+        });
+      } else {
+        if( !form1dirty && !form2dirty ){
+          this.location.back();
+        }
+      }
+    }  
   }
 }
